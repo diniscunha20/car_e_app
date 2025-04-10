8b0f0312b39beeb9780bb5c983f10976a73309f9
diniscunha20 <diniscunha20@ua.pt>
2025-04-10 12:48:22 +0100

    tux

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import ultratuxes from './assets/LOGO_TUX_V3(1).png'
 import './App.css'
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://www.instagram.com/desporto_nei/" target="_blank">
+          <img src={ultratuxes} className="logo tux" alt="tux logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
