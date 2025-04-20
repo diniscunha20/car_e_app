b554d07e2df035daad2ad80a96600b50b296d43d
GabrielFcGoncalves <gabfacgon3@gmail.com>
2025-04-20 20:26:16 +0100

    Pombal go cook

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -1,16 +1,35 @@
-function CarCard({ name }: { name?: string }) {
+import React from 'react';
+import "../assets/css/CarCard.css"
+
+type Carro = {
+    marca: string;
+    modelo: string;
+    matricula: string;
+    eventos: string[];
+    cor_rgb: number[]; // relax this type to avoid strict length checking
+    data_fabrico: string;
+    imagem_url: string;
+};
+
+
+const CarCard: React.FC<{ carro: Carro }> = ({ carro }) => {
     return (
     <div className="card h-60 bg-gradient-to-bl from-stone-700 to-stone-900 rounded-3xl mb-2 overflow-hidden shadow-xl" >
         <div>
             <h2 className="card-title font-medium text-2xl">
-            {name ? name : "Car Name"}
+            {carro.marca}
             </h2>
+            <h1 className="card-title font-medium text-2xl" >{carro.modelo}</h1>
         </div>
+
+        
         <div className="w-full h-full flex-col bg-gradient-to-bl from-stone-900 to-stone-700  rounded-2xl text-amber-100 p-4 border-2 text-left mt-2">
             <div>
                 <p className="mt-auto">A card component has a figure, a body part, and inside body there are title and actions parts</p>
             </div>
         </div>
+        <img src={carro.imagem_url} className="w-full h-40 object-cover rounded-t-3xl" />
+
     </div>
     );
        
--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,19 +1,26 @@
 import CarCard from '../components/CarCard'
 import NavBar from '../components/NavBar'
 import '../App.css'
+import carrosData from '../assets/carros.json'
 
 function HomePage() {
+  const carros = carrosData.carros;
+
   return (<>
-  <h1 className = 'text-2xl font-light text-left text-black'>Welcome,</h1>
-  <h1 className = 'text-4xl font-extrabold text-left text-black mb-3'>Claudino.</h1>
-  <CarCard name = 'Renault Clio'/>
-  <CarCard name = 'Fiat Tipo'/>
-  <CarCard name = 'Renault Twingo'/>
-  <NavBar/>
-  {/* <Avatar name = 'tira tira que eu vou cagar'/> */}
+    <h1 className = 'text-2xl font-light text-left text-black'>Welcome,</h1>
+    <h1 className = 'text-4xl font-extrabold text-left text-black mb-3'>Claudino.</h1>
+  
+    {carros.map((carro, index) => (
+    
+        <CarCard key={index} carro={carro}/>
+      
+    ))}
+    <NavBar/>
+    {/* <Avatar name = 'tira tira que eu vou cagar'/> */}
   </>)
 
 
 }
 
 export default HomePage
+
