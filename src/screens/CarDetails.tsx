b81fe70e80fc21fcd99e92bfd0cb9119718048bc
GabrielFcGoncalves <gabfacgon3@gmail.com>
2025-05-05 10:23:31 +0100

    Pass pro pombas

--- src/screens/CarDetails.tsx
+++ src/screens/CarDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import carrosData from '../assets/carros.json';
+import NavBar from '../components/NavBar';
 
 // Define types for the Carro object
 interface Carro {
@@ -25,14 +26,32 @@ const CarDetails: React.FC = () => {
     console.log("car:", carro)
   }
 
+  const carColor = `rgb(${carro.cor_rgb.join(', ')})`;
+
   return (
-    <div className="car-details">
-      <h2>Details for {carro.marca} {carro.modelo}</h2>
-      <p><strong>Matricula:</strong> {carro.matricula}</p>
-      <p><strong>Data de Fabrico:</strong> {carro.data_fabrico}</p>
-      <p><strong>Eventos:</strong> {carro.eventos.join(', ')}</p>
-      <p><strong>Color:</strong> RGB({carro.cor_rgb.join(', ')})</p>
-      <img src={carro.imagem_url} alt={`${carro.marca} ${carro.modelo}`} />
+    <div className="text-black h-full w-full"> {/* Add text-black here */}
+      
+      <div className="car-details h-11/12">
+            
+            <div className='h-1/4'>
+
+            </div>
+
+            <div className='h-3/4' style={{ backgroundColor: carColor }}>
+                <h2 className="text-2xl font-bold text-black">Details for {carro.marca} {carro.modelo}</h2>
+                <p className="text-black"><strong>Matricula:</strong> {carro.matricula}</p>
+                <p className="text-black"><strong>Data de Fabrico:</strong> {carro.data_fabrico}</p>
+                <p className="text-black"><strong>Eventos:</strong> {carro.eventos.join(', ')}</p>
+                <p className="text-black"><strong>Color:</strong> RGB({carro.cor_rgb.join(', ')})</p>
+                <img src={carro.imagem_url} alt={`${carro.marca} ${carro.modelo}`} />
+            </div>
+            
+      </div>
+
+      <div className='h-1/12'>
+        <NavBar />
+      </div>
+      
     </div>
   );
 }
