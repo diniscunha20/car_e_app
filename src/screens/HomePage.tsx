bb28814f5f67446624c57a665edaa9e7ea2a9c89
diniscunha20 <diniscunha20@ua.pt>
2025-05-14 22:14:34 +0100

    correcao scroll home page

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -34,42 +34,44 @@ function HomePage() {
   }, []);
 
   return (
-    <div className='h-full px-3 mt-2'>
-      <h1 className="text-2xl font-light text-left text-black">Welcome,</h1>
-      <h1 className="text-4xl font-extrabold text-left text-black mb-3">
-        {localStorage.getItem('username') || 'Guest'}
-      </h1>
+    <div>
+      <div className='h-full px-3 mt-2 overflow-y-scroll'>
+        <h1 className="text-2xl font-light text-left text-black">Welcome,</h1>
+        <h1 className="text-4xl font-extrabold text-left text-black mb-3">
+          {localStorage.getItem('username') || 'Guest'}
+        </h1>
 
-      <div className="h-full pb-40"> {/* leave space for NavBar */}
-        {carros.map((carro, index) => (
-          <Link key={index} to={`/car-details/${carro.matricula}`}>
-            <CarCard carro={carro}/>
-          </Link>
-        ))}
-      </div>
-
-      {/* Add Button */}
-      <button
-        className="fixed bottom-20 right-4 z-50 h-10 w-15 bg-stone-900 text-[#F8F8E0] text-3xl rounded-full shadow-xl hover:bg-stone-800 transition border-2"
-        onClick={() => setShowModal(true)}
-      >
-        +
-      </button>
-
-      {/* Modal */}
-      {showModal && (
-        <div className="fixed inset-0 z-40 bg-black/70 flex justify-center items-center">
-          <div className="relative z-50">
-            <button
-              className="absolute top-2 right-2 text-xl text-white font-bold bg-red-600 rounded-full w-8 h-8"
-              onClick={() => setShowModal(false)}
-            >
-              ×
-            </button>
-            <AddCarModal />
-          </div>
+        <div className="h-[80%] pb-15">
+          {carros.map((carro, index) => (
+            <Link key={index} to={`/car-details/${carro.matricula}`}>
+              <CarCard carro={carro}/>
+            </Link>
+          ))}
         </div>
-      )}
+
+        {/* Add Button */}
+        <button
+          className="fixed bottom-20 right-4 z-50 h-10 w-15 bg-stone-900 text-[#F8F8E0] text-3xl rounded-full shadow-xl hover:bg-stone-800 transition border-2"
+          onClick={() => setShowModal(true)}
+        >
+          +
+        </button>
+
+        {/* Modal */}
+        {showModal && (
+          <div className="fixed inset-0 z-40 bg-black/70 flex justify-center items-center">
+            <div className="relative z-50">
+              <button
+                className="absolute top-2 right-2 text-xl text-white font-bold bg-red-600 rounded-full w-8 h-8"
+                onClick={() => setShowModal(false)}
+              >
+                ×
+              </button>
+              <AddCarModal />
+            </div>
+          </div>
+        )}
+      </div>
 
       <NavBar />
     </div>
