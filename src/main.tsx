2897bfba23f140f8c7ba7d25a428322c55d29a3f
GabrielFcGoncalves <gabfacgon3@gmail.com>
2025-05-27 08:51:31 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import 'leaflet/dist/leaflet.css';
-import ProgressProvider from "./components/ProgressProvider";
+
 
 createRoot(document.getElementById('root')!).render(
-    <div>
-
     <App />
-    <ProgressProvider />
-    </div>
 
 )
